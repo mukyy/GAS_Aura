2f55528e79540bda9ae305cb02887de933d4f1b6
mukyy <39865405+mukyy@users.noreply.github.com>
2024-03-14 11:33:51 +0100

    - Base character class created. - Project converted to cpp.

---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Emircan Birbil
+
+#pragma once
+
+#include "CoreMinimal.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Emircan Birbil
+
+
+#include "AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
